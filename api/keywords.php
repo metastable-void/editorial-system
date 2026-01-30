901d9c6a53586006ef78b4f58656768893ac5a6e
metastable-void <git@yuka.h09.eu>
2026-01-30 09:58:43 +0900

    keywords list

---
+++ api/keywords.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace innovatopia_jp\editorial;
+
+require_once __DIR__ . '/../includes/init.php';
+
+function json_response(array $data, int $status = 200): never {
+    if (!\headers_sent()) {
+        \header('Content-Type: application/json');
+        \http_response_code($status);
+    }
+    echo \json_encode($data);
+    exit;
+}
+
+try {
+    if (($_SERVER['REQUEST_METHOD'] ?? 'GET') !== 'GET') {
+        json_response(['error' => 'Method not allowed.'], 405);
+    }
+    $model = new Model(Config::getMysqlConfig(), Config::getOpenAiConfig());
+    $keywords = $model->get_unique_keywords();
+    json_response(['keywords' => $keywords]);
+} catch (\Throwable $error) {
+    json_response(['error' => $error->getMessage()], 400);
+}
