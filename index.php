f2407e300c0933c492c2fb5c2e8d2a386df8b143
metastable-void <git@yuka.h09.eu>
2026-01-30 11:19:35 +0900

    favicon

--- index.php
+++ index.php
@@ -9,5 +9,6 @@ require_once __DIR__ . '/includes/init.php';
 <meta charset="UTF-8">
 <title>編集部システム</title>
 <link rel="stylesheet" href="/styles.css">
+<link rel="icon" href="/favicon.ico">
 <div id="main"></div>
 <script src="/main.js"></script>
